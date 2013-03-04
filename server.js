a1ff34916b6b82a13abbac978cfd1b3335a80a5e
James Halliday <mail@substack.net>
2013-03-04 08:40:40 -0800

    basic glog server and header

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var glog = require('glog')(process.argv[3]);
+var ecstatic = require('ecstatic')(__dirname + '/static');
+
+var qs = require('querystring');
+
+var server = http.createServer(function (req, res) {
+    if (glog.test(req.url)) return glog(req, res);
+    
+    if (/^\/[^\.\/]+$/.test(req.url)) {
+        req.url = '/';
+    }
+    ecstatic(req, res);
+});
+server.listen(Number(process.argv[2]));
