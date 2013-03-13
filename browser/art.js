61a8726eef83e2c5c4da806bf91fa531e724965c
James Halliday <mail@substack.net>
2013-03-13 01:38:28 -0700

    more link on the art page

--- browser/art.js
+++ browser/art.js
@@ -5,42 +5,60 @@ module.exports = function () { return new Art };
 
 function Art () {
     var self = this;
+    self.root = document.createElement('div');
     self.element = document.createElement('div');
-    self.pageIndex = 0;
-    self.pages = [];
+    self.root.appendChild(self.element);
+    
+    var more = self.moreLink = document.createElement('div');
+    more.className = 'more';
+    more.appendChild(document.createTextNode('more'));
+    more.addEventListener('click', function (ev) {
+        ev.preventDefault();
+        self.more();
+    });
+    
+    self.root.appendChild(more);
+    
+    self.limit = 0;
+    self.showing = 0;
     
     loadImages(function (images) {
-        images.forEach(function (src, ix) {
-            var i = ix % 10;
-            var j = Math.floor(ix / 10);
-            var page = self.pages[j];
-            if (!page) {
-                page = self.pages[j] = document.createElement('div');
-                page.className = 'page';
-                if (j === self.pageIndex) page.className += ' active';
-                self.element.appendChild(page);
-            }
-            
-            var link = document.createElement('a');
-            link.setAttribute('href', '/images/' + src);
-            
-            var img = document.createElement('img');
-            img.setAttribute('src', '/images/' + src);
-            
-            link.appendChild(img);
-            page.appendChild(link);
-        });
+        self.images = images;
+        self.more();
     });
 }
 
 Art.prototype.appendTo = function (target) {
     var self = this;
     if (typeof target === 'string') target = document.querySelector(target);
-    target.appendChild(self.element);
+    target.appendChild(self.root);
 };
 
-Art.prototype.next = function () {};
-Art.prototype.prev = function () {};
+Art.prototype.more = function () {
+    var self = this;
+    if (!self.moreLink) return;
+    
+    self.limit += 10;
+    self.images.slice(self.showing, self.limit)
+        .forEach(function (src) { self.render(src) })
+    ;
+    self.showing += 10;
+    if (self.showing >= self.images.length) {
+        self.root.removeChild(self.moreLink);
+        self.moreLink = null;
+    }
+};
+
+Art.prototype.render = function (src) {
+    var link = document.createElement('a');
+    link.setAttribute('href', '/images/' + src);
+    
+    var img = document.createElement('img');
+    img.setAttribute('src', '/images/' + src);
+    link.appendChild(img);
+    
+    this.element.appendChild(link);
+};
 
 function loadImages (cb) {
     var r = hyperquest('http://' + window.location.host + '/images.json');
