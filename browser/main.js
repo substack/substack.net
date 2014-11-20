705b1e57465a31497df7086ccf59152d44609b66
James Halliday <mail@substack.net>
2014-11-20 15:34:24 +0100

    colorful banner

--- browser/main.js
+++ browser/main.js
@@ -1,4 +1,5 @@
 var classList = require('class-list');
+var colorname = require('colornames');
 
 var avatar = document.getElementById('avatar');
 avatar.addEventListener('mouseover', function (ev) {
@@ -82,12 +83,39 @@ if (banner) (function () {
     }
     
     var offset = 0;
-    var colors = [ 'cyan', 'orange', 'magenta', 'gold' ];
+    var colors = [ 'cyan', 'magenta', 'orange', 'lime' ];
+    var colorx = [];
+    for (var i = 0; i < colors.length; i++) {
+        var a = phex(colorname(colors[i]));
+        var b = phex(colorname(colors[(i+1)%colors.length]));
+        var steps = 10;
+        for (var j = 0; j < steps; j++) {
+            var ja = 1 - j/steps, jb = j/steps;
+            colorx.push(shex([
+                a[0] * ja + b[0] * jb,
+                a[1] * ja + b[1] * jb,
+                a[2] * ja + b[2] * jb
+            ]));
+        }
+    }
+    console.log(colorx);
     
     setInterval(function () {
         for (var i = 0; i < elems.length; i++) {
-            elems[i].style.color = colors[Math.abs(i - offset) % colors.length];
+            var ix = Math.abs(i - offset) % colorx.length;
+            elems[i].style.color = colorx[ix];
         }
         offset ++;
     }, 50);
 })();
+
+function phex (s) {
+    return s.slice(1).split(/(\w\w)/).filter(Boolean)
+        .map(function (x) { return parseInt(x,16) })
+    ;
+}
+
+function shex (xs) {
+    return '#' + xs.map(tos).join('');
+    function tos (x) { return (x < 16 ? '0' : '') + Math.floor(x).toString(16) }
+}
